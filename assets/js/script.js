b3929feb5518bfcc81361996dbfaa09e9ac47de0
Jose Lorenzo L. Victorino <josevictorino003@gmail>
2024-07-24 22:38:12 +0800

    coalescingHole(), bestFit, worstFit <added>

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,7 +35,7 @@ form.addEventListener('submit', e => {
   e.preventDefault();
 
   const memorySize = form.querySelector('#memorySize').value;
-  const algorithm = form.querySelector('#algorithm').value;
+  const algorithm = form.querySelector('[name="algorithm"]:checked').value;
   const timeCompaction = form.querySelector('#timeCompaction').value;
   const coalescingHole = form.querySelector('#coalescingHole').value;
   const TRs = tbody.children;
@@ -59,9 +59,9 @@ form.addEventListener('submit', e => {
     }
   }
 
-  given.memorySize = parseInt(memorySize);
-  given.timeCompaction = parseInt(timeCompaction);
-  given.coalescingHole = parseInt(coalescingHole);
+  given.memorySize = memorySize * 1000;
+  given.timeCompaction = timeCompaction;
+  given.coalescingHole = coalescingHole;
   given.algorithm = algorithm;
 
   startSimulation();
@@ -72,22 +72,43 @@ async function startSimulation(){
 
   while(totalTimeUnitsLeft() > 0){
     updateJobsInMemory();
+    for(let job of given.jobs){
+      const {number, size, allocated, done} = job;
+  
+      if(done) continue;
+  
+      if(!allocated){
+        switch(given.algorithm){
+          case 'First Fit':
+            firstFitAllocate(job);
+            break;
+          case 'Best Fit':
+            bestFitAllocate(job);
+            break;
+          case 'Worst Fit':
+            worstFitAllocate(job);
+            break;
+        }
+      }
+  
+      updateJobsInMemory();
 
-    switch(given.algorithm){
-      case 'First Fit':
-        await firstFitAlgorithm();
-        break;
-      case 'Best Fit':
-        await bestFitAlgorithm();
-        break;
-      case 'Worst Fit':
-        await worstFitAlgorithm();
-        break;
+      if(job.allocated){
+        const isDeallocated = await updateContent(job);
+    
+        await delay();
+        if(isDeallocated){
+          await allocatePendingJobs();
+        }
+        if(given.currentTimeUnit % given.timeCompaction === 0){
+          await timeCompaction();
+        }
+      }
     }
   }
 }
 
-const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms || 1200));
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms || 1000));
 const totalTimeUnitsLeft = () => given.jobs.reduce((total, job) => total + job.timeUnitLeft, 0);
 function generateContent(){
   userInput.style.display = 'none';
@@ -128,6 +149,7 @@ function generateContent(){
   timeUnitTable.innerHTML = content;
   memory.style.gridTemplateRows = `repeat(${given.memorySize}, 1fr)`;
 }
+// Sort Allocated Jobs
 function updateJobsInMemory(){
   jobsInMemory = Array.from(memory.querySelectorAll('div'));
 
@@ -138,38 +160,12 @@ function updateJobsInMemory(){
   });
   jobsInMemory.forEach(div => memory.appendChild(div));
 }
-
 function getBounds(element){
   const bounds = element.style.gridRow.split('/');
   return {start: parseInt(bounds[0]), end: parseInt(bounds[1])};
 }
 
 // First-Fit Algorithm
-async function firstFitAlgorithm(){
-  for(let job of given.jobs){
-    const {number, size, allocated, done} = job;
-
-    if(done) continue;
-
-    if(!allocated){
-      firstFitAllocate(job);
-    }
-
-    updateJobsInMemory();
-
-    if(job.allocated){
-      const res = await updateContent(job);
-
-      await delay();
-      if(res){
-        await allocatePendingJobs();
-      }
-      if(given.currentTimeUnit % given.timeCompaction === 0){
-        await timeCompaction();
-      }
-    }
-  }
-}
 function firstFitAllocate(job){
   const {number, size} = job;
 
@@ -181,7 +177,7 @@ function firstFitAllocate(job){
     let start, end;
 
     for(let i = 0; i <= jobsInMemory.length; i++){
-      if (i === 0){
+      if(i === 0){
         start = 0;
         end = getBounds(jobsInMemory[i]).start;
       }
@@ -202,20 +198,103 @@ function firstFitAllocate(job){
   }
 }
 // Best-Fit Algorithm
-async function bestFitAlgorithm(){
-
-}
 function bestFitAllocate(job){
+  const {number, size} = job;
+  let bestFitIndex = -1;
+  let bestFitSize = Infinity;
 
+  for(let i = 0; i <= jobsInMemory.length; i++){
+    let start, end;
+
+    if(i === 0){
+      start = 0;
+      end = jobsInMemory.length > 0 ? getBounds(jobsInMemory[i]).start : given.memorySize;
+    }
+    else if(i === jobsInMemory.length){
+      start = getBounds(jobsInMemory[i - 1]).end;
+      end = given.memorySize;
+    }
+    else{
+      start = getBounds(jobsInMemory[i - 1]).end;
+      end = getBounds(jobsInMemory[i]).start;
+    }
+
+    const holeSize = end - start;
+    if(size <= holeSize && holeSize < bestFitSize){
+      bestFitIndex = i;
+      bestFitSize = holeSize;
+    }
+  }
+
+  if(bestFitIndex !== -1){
+    let start, end;
+    if(bestFitIndex === 0){
+      start = 0;
+      end = jobsInMemory.length > 0 ? getBounds(jobsInMemory[bestFitIndex]).start : given.memorySize;
+    }
+    else if(bestFitIndex === jobsInMemory.length){
+      start = getBounds(jobsInMemory[bestFitIndex - 1]).end;
+      end = given.memorySize;
+    }
+    else{
+      start = getBounds(jobsInMemory[bestFitIndex - 1]).end;
+      end = getBounds(jobsInMemory[bestFitIndex]).start;
+    }
+
+    memory.innerHTML += `<div class="J${number}" style="grid-row: ${(start === 0) ? 1 : start}/${start + size};">J${number} - ${size}</div>`;
+    job.allocated = true;
+  }
 }
 // Worst-Fit Algorithm
-async function worstFitAlgorithm(){
-
-}
 function worstFitAllocate(job){
+  const {number, size} = job;
+  let worstFitIndex = -1;
+  let worstFitSize = -1;
 
+  for(let i = 0; i <= jobsInMemory.length; i++){
+    let start, end;
+
+    if(i === 0){
+      start = 0;
+      end = jobsInMemory.length > 0 ? getBounds(jobsInMemory[i]).start : given.memorySize;
+    }
+    else if(i === jobsInMemory.length){
+      start = getBounds(jobsInMemory[i - 1]).end;
+      end = given.memorySize;
+    }
+    else{
+      start = getBounds(jobsInMemory[i - 1]).end;
+      end = getBounds(jobsInMemory[i]).start;
+    }
+
+    const holeSize = end - start;
+    if(size <= holeSize && holeSize > worstFitSize){
+      worstFitIndex = i;
+      worstFitSize = holeSize;
+    }
+  }
+
+  if(worstFitIndex !== -1){
+    let start, end;
+    if(worstFitIndex === 0){
+      start = 0;
+      end = jobsInMemory.length > 0 ? getBounds(jobsInMemory[worstFitIndex]).start : given.memorySize;
+    }
+    else if(worstFitIndex === jobsInMemory.length){
+      start = getBounds(jobsInMemory[worstFitIndex - 1]).end;
+      end = given.memorySize;
+    }
+    else{
+      start = getBounds(jobsInMemory[worstFitIndex - 1]).end;
+      end = getBounds(jobsInMemory[worstFitIndex]).start;
+    }
+
+    memory.innerHTML += `<div class="J${number}" style="grid-row: ${(start === 0) ? 1 : start}/${start + size};">J${number} - ${size}</div>`;
+    job.allocated = true;
+  }
 }
-// Allocating Jobs When A Job is Deallocated
+
+// Allocate Pending Jobs When A Job is Deallocated
 async function allocatePendingJobs(){
   for (let job of given.jobs) {
     const {allocated, done} = job;
@@ -241,22 +320,22 @@ async function allocatePendingJobs(){
     updateJobsInMemory();
   }
 }
-
 // Updating UI Elements Every Time Unit
 async function updateContent(job){
   const {number, size, timeUnit, timeUnitLeft, allocated, done} = job;
   const cell = timeUnitTable.querySelector(`tbody tr:nth-child(${number}) .TU`);
   let deallocate = false;
+  let jobDiv = undefined;
   
   cell.classList.remove('TU');
   if(--job.timeUnitLeft === 0){
     cell.innerText = '*';
     job.done = true;
 
-    const jobDiv = memory.querySelector(`.J${number}`);
+    jobDiv = memory.querySelector(`.J${number}`);
     if(jobDiv){
-      memory.removeChild(jobDiv);
       deallocate = true;
+      memory.removeChild(jobDiv);
     }
   }
   else{
@@ -265,6 +344,10 @@ async function updateContent(job){
   flow.innerHTML += `<div>J${number}</div>`;
   given.currentTimeUnit++;
 
+  if(typeof jobDiv !== 'undefined' && deallocate && given.currentTimeUnit % given.coalescingHole === 0){
+    await coalescingHole(jobDiv);
+  }
+
   return deallocate;
 }
 
@@ -272,16 +355,53 @@ async function updateContent(job){
 async function timeCompaction(){
   let start = 1;
 
-  for(let i = 0; i < jobsInMemory.length; i++){
-    const bounds = jobsInMemory[i].style.gridRow.split('/');
-    const size = parseInt(bounds[1]) - parseInt(bounds[0]);
+  for(let i = 0; i <= jobsInMemory.length; i++){
+    if(i < jobsInMemory.length){
+      const bounds = jobsInMemory[i].style.gridRow.split('/');
+      const size = parseInt(bounds[1]) - parseInt(bounds[0]);
     
-    jobsInMemory[i].style.gridRow = `${start}/${start + size}`;
-    start += size;
-
-    given.currentTimeUnit++;
+      jobsInMemory[i].style.gridRow = `${start}/${start + size}`;
+      start += size;
+  
+      given.currentTimeUnit++;
+    }
 
     flow.innerHTML += `<div>SC</div>`;
     await delay();
+  }
+}
+// Coalescing Hole Function
+async function coalescingHole(jobDiv){
+  if(jobsInMemory.length === 1) return;
+  for(let i = 0; i < jobsInMemory.length; i++){
+    let coalesce = false, boundA1, boundB1, boundA2, boundB2;
+
+    if(jobsInMemory[i] === jobDiv){
+      if(i === 0){
+        boundA1 = getBounds(jobsInMemory[i]).end;
+        boundB1 = getBounds(jobsInMemory[i + 1]).start;
+        if(boundA1 < boundB1)
+          coalesce = true;
+      }
+      else if(i === jobsInMemory.length - 1){
+        boundA1 = getBounds(jobsInMemory[i]).start;
+        boundB1 = getBounds(jobsInMemory[i - 1]).end;
+        if(boundA1 > boundB1)
+          coalesce = true;
+      }
+      else{
+        boundA1 = getBounds(jobsInMemory[i]).start;
+        boundA2 = getBounds(jobsInMemory[i]).end;
+        boundB1 = getBounds(jobsInMemory[i - 1]).end;
+        boundB2 = getBounds(jobsInMemory[i + 1]).start;
+        if(boundA1 > boundB1 || boundA2 < boundB2)
+          coalesce = true;
+      }
+    }
+    if(coalesce){
+      await delay();
+      given.currentTimeUnit++;
+      flow.innerHTML += `<div>CH</div>`;
+    }
   }
 }
